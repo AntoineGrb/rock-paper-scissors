d07774945e8f915351a313025744876e6114816e
Antoine Grubert <antoine_grubert@hotmail.fr>
2023-01-05 13:25:40 +0100

    V3.1 - Début du code JS

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-let test = "Test console";
-console.log(test + "complément");
 
-let test2 = 30;
-console.log(test2 + 40);
+/*----------- Variables & constantes ----------------- */
+const handsArray = ["rock" , "paper" , "scissors"];
+let userScore = 0;
+let iaScore = 0;
+let winGame = "";
+
+
+/*----------- Définition des deux fonctions de l'IA ----------------- */
+function random() {
+
+let handsIndex = Math.floor(Math.random()*handsArray.length);
+let iaHand = handsArray[handsIndex];
+return iaHand;
+
+}
+
+/* Fonction expert à définir */
+
+
+/*----------------- Code ----------------- */
+
+/* 0 - Reset les variables */
+let userHand = "";
+let iaHand = "";
+
+let test = document.getElementById("zone__test");
+test.innerHTML="<p> balise </p>";
+
+/* 1 - Déclenchement de l'évènement */
+
+/* document.querySelector(".user__board__select__rock")
+        .addEventListener("click" , function(event) {
+
+            event.stopPropagation();
+            event.preventDefault();
+            
+            userHand = "rock";
+            document.querySelector(".zone__test")
+                    .innerHTML = userHand;
+
+        }); */
